35c92b0e8c25bf163ae0bc5d94ab5a0ae9ef2402
Progra190 <99901442+Progra190@users.noreply.github.com>
2022-04-08 16:49:02 -0300

    fghjk

---
+++ header.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vittug</title>
+</head>
+<body>
+    <div class="wrap">
+        <header>
+            <a href="/"><img src="/Imagem/Baloes.jpg" alt=" Logotipo do Vittug "> </a>
+           <h1> Vittug <small>Dinheiro sempre Dinheiro</small></h1>
+        </header>    
+            <nav>
+                <a href="/"><i class="fa-solid fa-house-chimney"></i><span>Início</span></a>
+                <a href="/contatos" title="Faça contato"><i class="fa-solid fa-commient-dots"></i><span>Contatos</span></a>
+                <a href="/sobre" title="Sobre..."><i class="fa-solid fa-circle-info"></i><span>Sobre</span></a>
+                <a href="/usuario" title="Usuário"><i class="fa-solid fa-user"></i><span>Usuário</span></a>
+            </nav>
+                <main>
